60380e082fb1244fa4370552de05bb11d623311a
Homeash Pranav <93364869+Homeash-github@users.noreply.github.com>
2023-07-25 22:47:50 +0530

    Date and Time

---
+++ dnt.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class dnt {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int month = scanner.nextInt();
+            int day = scanner.nextInt();
+            int year = scanner.nextInt();
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.DATE, day);
+            calendar.set(Calendar.MONTH, month - 1);
+            calendar.set(Calendar.YEAR, year);
+
+            System.out.println(calendar.getDisplayName(Calendar.DAY_OF_WEEK,
+                    Calendar.LONG, new Locale("en", "US")).toUpperCase());
+        }
+    }
+}
