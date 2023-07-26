683e167cce5fd0288cbfd1206505eb89b670db32
Homeash Pranav <93364869+Homeash-github@users.noreply.github.com>
2023-07-26 23:21:05 +0530

    Add files via upload

---
+++ pat2.java
@@ -0,0 +1,12 @@
+public class pat2 {
+    public static void main(String[] args) {
+        for(int i =1;i<5;i++)
+        {
+            for(int j=1;j<i+1;j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
