ea3102365dae4f99d225b55348aaeaa4c4eede7f
Homeash Pranav <93364869+Homeash-github@users.noreply.github.com>
2023-08-03 23:24:23 +0530

    Add files via upload

---
+++ duck.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class duck
+{
+    public static void main(String[] args)
+    {
+        int r, n, num;
+        boolean flag=false;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        n = sc.nextInt();
+        num = n;
+        while (num > 0)
+        {
+            r = num % 10;
+            if(r==0)
+            {
+                flag=true;
+            }
+            num = num / 10;
+        }
+        if(flag)
+        {
+            System.out.println("Duck Number");
+        }
+        else
+        {
+            System.out.println("Not Duck Number");
+        }
+
+    }
+}
