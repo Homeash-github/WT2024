1f723c2952692f5c58bb6948bb29d5ac3d77ae83
Homeash Pranav V <93364869+Homeash-github@users.noreply.github.com>
2024-07-04 14:14:00 +0530

    Update duplicate_values.java

--- duplicate_values.java
+++ duplicate_values.java
@@ -1,24 +1,3 @@
-// import java.util.*;
-// public class duplicate_values
-// {
-// 	public static void main(String[] args) {
-// 		Scanner sc = new Scanner(System.in);
-// 		int arr[]={10,20,45,18,49,96,18,45,49,33};
-// 		System.out.println("Duplicate elements are:",findDuplicates(arr));
-// 	}
-// }
-
-// public static ArrayList<Integer> findDuplicates(int[] arr) {
-//   ArrayList<Integer> duplicates = new ArrayList<>();
-//   for (int i = 0; i < arr.length; i++) {
-//     for (int j = i + 1; j < arr.length; j++) {
-//       if (arr[i] == arr[j] && !duplicates.contains(arr[j])) {
-//         duplicates.add(arr[j]);
-//       }
-//     }
-//   }
-//   return duplicates;
-// }
 import java.util.*;
 public class duplicate_values {
 
